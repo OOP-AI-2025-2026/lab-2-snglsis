394f1f562b4ef8c5df44a2b3c1407d612f576adb
angelinaprohorova <linaprokhorova07@gmail.com>
2025-09-21 15:02:48 +0300

    Completed Student class assignment

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,5 +2,14 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
+        Student student = new Student("Олександр", 3);
+
+        student.addCourse("Математика");
+        student.addCourse("Фізика");
+        student.addCourse("Інформатика");
+
+        System.out.println(student.getName() + ": кількість вивчаємих дисциплін - " + student.getCourseCount());
+        System.out.println(student.getName() + ": рік навчання - " + student.getYear());
+        System.out.println(student.getName() + ": заплатив за навчання - " + student.getTuition() + " грн");
     }
 }
--- src/main/java/ua/opnu/TimeSpan.java
+++ src/main/java/ua/opnu/TimeSpan.java
@@ -4,43 +4,69 @@ package ua.opnu;
 public class TimeSpan {
 
     // TODO: add class fields
+    private int hours;
+    private int minutes;
 
-    TimeSpan(int hours, int minutes) {
-        // TODO: write constructor body
+    public TimeSpan(int hours, int minutes) {
+        if (hours < 0 || minutes < 0 || minutes > 59) {
+            throw new IllegalArgumentException("Некоректні значення годин або хвилин");
+        }
+        this.hours = hours;
+        this.minutes = minutes;
     }
 
-    int getHours() {
-        return 0;
+    public int getHours() {
+        return hours;
     }
 
-    int getMinutes() {
-        // TODO: write method body
-        return 0;
+    public int getMinutes() {
+        return minutes;
     }
+
 
     void add(int hours, int minutes) {
-        // TODO: write method body
+        if (hours < 0 || minutes < 0 || minutes > 59) {
+            throw new IllegalArgumentException("Некоректні значення для додавання");
+        }
+        this.hours += hours;
+        this.minutes += minutes;
+        if (this.minutes >= 60) {
+            this.hours += this.minutes / 60;
+            this.minutes = this.minutes % 60;
+        }
     }
 
     void addTimeSpan(TimeSpan timespan) {
-        // TODO: write method body
+        add(timespan.getHours(), timespan.getMinutes());
     }
 
     double getTotalHours() {
-        // TODO: write method body
-        return 0;
+        return hours + minutes / 60.0;
     }
 
     int getTotalMinutes() {
-        // TODO: write method body
-        return 0;
+        return hours * 60 + minutes;
     }
 
     void subtract(TimeSpan span) {
-        // TODO: write method body
+        int totalMinutesCurrent = getTotalMinutes();
+        int totalMinutesSubtract = span.getTotalMinutes();
+
+        if (totalMinutesSubtract > totalMinutesCurrent) {
+            throw new IllegalArgumentException("Віднімання неможливе: інтервал більший за поточний");
+        }
+
+        int resultMinutes = totalMinutesCurrent - totalMinutesSubtract;
+        this.hours = resultMinutes / 60;
+        this.minutes = resultMinutes % 60;
     }
 
     void scale(int factor) {
-        // TODO: write method body
+        if (factor <= 0) {
+            throw new IllegalArgumentException("Factor має бути > 0");
+        }
+        int totalMinutes = getTotalMinutes() * factor;
+        this.hours = totalMinutes / 60;
+        this.minutes = totalMinutes % 60;
     }
 }
